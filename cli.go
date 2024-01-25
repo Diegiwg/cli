f545f7026e833d982daf10e8ba9772d087a13458
Diegiwg <diegiwg@gmail.com>
2024-01-24 21:42:12 -0300

    update

--- cli.go
+++ cli.go
@@ -1,5 +1,100 @@
 package cli
 
-func Test() {
-	println("CLI Lib installed and working!")
+import (
+	"errors"
+	"os"
+)
+
+type Context struct {
+	App   *App
+	Args  []string
+	Flags map[string]string
+}
+
+type Command struct {
+	Name string
+	Desc string
+	Help string
+	Exec func(ctx *Context) error
+}
+
+type App struct {
+	Program         string
+	Args            []string
+	ArgsPtr         int
+	DefaultRoutine  func(ctx *Context) error
+	SelectedCommand *Command
+	Commands        map[string]*Command
+}
+
+func NewApp() *App {
+	return &App{
+		Program:         os.Args[0],
+		Args:            os.Args[1:],
+		ArgsPtr:         0,
+		DefaultRoutine:  nil,
+		SelectedCommand: nil,
+		Commands:        make(map[string]*Command),
+	}
+}
+
+func (app *App) Run() error {
+	// Parse the command
+	err := app.ParseCommand()
+	if err != nil && app.DefaultRoutine == nil {
+		return err
+	}
+
+	// Create the context
+	ctx := &Context{
+		App: app,
+	}
+
+	// Check if the SelectedCommand is Not nil
+	if app.SelectedCommand != nil {
+		return app.SelectedCommand.Exec(ctx)
+	}
+
+	// Check if the DefaultRoutine is Not nil
+	if app.DefaultRoutine != nil {
+		return app.DefaultRoutine(ctx)
+	}
+
+	return errors.New("unknown error")
+}
+
+func (app *App) SetDefaultRoutine(routine func(ctx *Context) error) {
+	app.DefaultRoutine = routine
+}
+
+func (app *App) AddCommand(cmd *Command) error {
+	// Check if the command already exists
+	if _, exists := app.Commands[cmd.Name]; exists {
+		return errors.New("command already exists")
+	}
+
+	app.Commands[cmd.Name] = cmd
+	return nil
+}
+
+func (app *App) RunCommand() error {
+	return nil
+}
+
+func (app *App) ParseCommand() error {
+	// Check if the Args is empty
+	if len(app.Args) == 0 {
+		return errors.New("no command provided")
+	}
+
+	// Check if the command exists
+	cmd, exists := app.Commands[app.Args[0]]
+	if !exists {
+		return errors.New("command not found")
+	}
+
+	// Set the SelectedCommand
+	app.ArgsPtr++
+	app.SelectedCommand = cmd
+	return nil
 }
--- example/main.go
+++ example/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "github.com/Diegiwg/cli"
+import (
+	"github.com/Diegiwg/cli"
+)
+
+func helpCommand(ctx *cli.Context) error {
+	println("Usage: " + ctx.App.Program + " <command> [arguments]")
+	return nil
+}
 
 func main() {
-	cli.Test()
+	app := cli.NewApp()
+
+	// app.SetDefaultRoutine(helpCommand)
+	app.AddCommand(&cli.Command{
+		Name: "help",
+		Desc: "Show this Help Message",
+		Exec: helpCommand,
+	})
+
+	err := app.Run()
+	if err != nil {
+		println(err.Error())
+	}
 }
